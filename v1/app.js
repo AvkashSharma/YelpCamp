8bf13c67034ff26ba3206320cddb81e03874eb75
Avkash <avkaashsharma89@gmail.com>
2020-06-14 16:28:16 -0400

    data read/write using mongoose. GET and POST

--- v1/app.js
+++ v1/app.js
@@ -3,21 +3,24 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yelp_camp', { useNewUrlParser: true });
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
 let campgrounds = [
 	{
 		name: 'Salmon Creek',
-		image: 'https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
+		image: 'https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e507440772f78d79748c7_340.jpg'
 	},
 	{
 		name: 'Granite Hill',
-		image: 'https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
+		image: 'https://pixabay.com/get/50e9d4474856b10ff3d8992ccf2934771438dbf85254784b752c78d4924f_340.jpg'
 	},
 	{
 		name: "Mountain Goat's Rest ",
-		image: 'https://pixabay.com/get/57e0d6424b56ad14f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
+		image: 'https://pixabay.com/get/52e8d4444255ae14f1dc84609620367d1c3ed9e04e507440772f78d79748c7_340.jpg'
 	}
 ];
 
--- v2/app.js
+++ v2/app.js
@@ -3,30 +3,48 @@ const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/yelp_camp', { useUnifiedTopology: true, useNewUrlParser: true });
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let campgrounds = [
-	{
-		name: 'Salmon Creek',
-		image: 'https://pixabay.com/get/57e8d1464d53a514f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
-	},
-	{
-		name: 'Granite Hill',
-		image: 'https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
-	},
-	{
-		name: "Mountain Goat's Rest ",
-		image: 'https://pixabay.com/get/57e0d6424b56ad14f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
-	}
-];
+// SCHEMA SETUP
+const campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+let Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+// 	{
+// 		name: 'Granite Hill',
+// 		image: 'https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
+// 	},
+// 	function(err, campground) {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log('NEWLY CREATED CAMPGROUND: ');
+// 			console.log(campground);
+// 		}
+// 	}
+// );
 
 app.get('/', (req, res) => {
 	res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-	res.render('campgrounds', { campgrounds: campgrounds });
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('campgrounds', { campgrounds: allCampgrounds });
+		}
+	});
 });
 
 app.post('/campgrounds', (req, res) => {
@@ -34,9 +52,16 @@ app.post('/campgrounds', (req, res) => {
 	let name = req.body.name;
 	let image = req.body.image;
 	let newCampground = { name: name, image: image };
-	campgrounds.push(newCampground);
-	// redirect to campgrounds page
-	res.redirect('/campgrounds');
+	// campgrounds.push(newCampground);
+	// Create a new campground and save to the DB
+	Campground.create(newCampground, function(err, newlyCreated) {
+		if (err) {
+			console.log(err);
+		} else {
+			// redirect to campgrounds page
+			res.redirect('/campgrounds');
+		}
+	});
 });
 
 app.get('/campgrounds/new', (req, res) => {
