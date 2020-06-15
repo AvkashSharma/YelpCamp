5f6515453468f9342958ec91e1b7522eb687e6c5
Avkash <avkaashsharma89@gmail.com>
2020-06-15 00:47:15 -0400

    Implemented show page and created a GET route by id

--- README.md
+++ README.md
@@ -15,3 +15,27 @@ YelpCamp
 ### Style the campgrounds page
 - Add a better header/title
 - Make campgrounds display in a grid
+
+### Add Mongoose 
+- Install and configure Mongoose 
+- Setup campground model
+- Use campground model inside of our routes
+
+### Show Page
+- Review the RESTful routes we've seen so far
+- Add description to out campground model
+- Show db.collection.drop()
+- Add a show route/template
+
+
+### RESTFUL ROUTES
+
+| Name  |     url         |   verb  |         description                |
+--------|:----------------|:-------:|:----------------------------------:|
+INDEX   |   /dogs         |   GET   |   Display a list of all dogs       |
+NEW     |   /dogs/new     |   GET   |   Displays form to make a new dog  |
+CREATE  |   /dogs         |   POST  |   Add new dog to DB                |
+SHOW    |   /dogs/:id     |   GET   |   Show info about one dog          |
+
+
+    
--- v2/app.js
+++ v2/app.js
@@ -12,7 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // SCHEMA SETUP
 const campgroundSchema = new mongoose.Schema({
 	name: String,
-	image: String
+	image: String,
+	description: String
 });
 
 let Campground = mongoose.model('Campground', campgroundSchema);
@@ -20,7 +21,8 @@ let Campground = mongoose.model('Campground', campgroundSchema);
 // Campground.create(
 // 	{
 // 		name: 'Granite Hill',
-// 		image: 'https://pixabay.com/get/57e8d1454b56ae14f1dc84609620367d1c3ed9e04e50744074267fd49648cd_340.jpg'
+// 		image: 'https://pixabay.com/get/54e5dc474355a914f1dc84609620367d1c3ed9e04e507440772e7add934ac5_340.jpg',
+// 		description: 'This is a huge granite hill, no bathrooms. No water. Beautiful granite!'
 // 	},
 // 	function(err, campground) {
 // 		if (err) {
@@ -36,22 +38,25 @@ app.get('/', (req, res) => {
 	res.render('landing');
 });
 
+// INDEX - show all campgrounds
 app.get('/campgrounds', (req, res) => {
 	// Get all campgrounds from DB
 	Campground.find({}, function(err, allCampgrounds) {
 		if (err) {
 			console.log(err);
 		} else {
-			res.render('campgrounds', { campgrounds: allCampgrounds });
+			res.render('index', { campgrounds: allCampgrounds });
 		}
 	});
 });
 
+// Create - add new campground to DB
 app.post('/campgrounds', (req, res) => {
 	// get data from form and add to campgrounds array
 	let name = req.body.name;
 	let image = req.body.image;
-	let newCampground = { name: name, image: image };
+	let desc = req.body.description;
+	let newCampground = { name: name, image: image, description: desc };
 	// campgrounds.push(newCampground);
 	// Create a new campground and save to the DB
 	Campground.create(newCampground, function(err, newlyCreated) {
@@ -64,9 +69,23 @@ app.post('/campgrounds', (req, res) => {
 	});
 });
 
+// NEW -  show form to create new campground
 app.get('/campgrounds/new', (req, res) => {
 	// render a new form
 	res.render('new.ejs');
+});
+
+// SHOW - shows more info about
+app.get('/campgrounds/:id', function(req, res) {
+	// find the campground with provided ID
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if (err) {
+			console.log(err);
+		} else {
+			// render show template with that campground
+			res.render('show', { campground: foundCampground });
+		}
+	});
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
