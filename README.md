b993ef76d7e6de9147abfbb1d21bf6601ff02f9f
Avkash <avkaashsharma89@gmail.com>
2020-06-23 11:43:40 -0400

    Show page setup, comments capacity set-up

--- README.md
+++ README.md
@@ -30,12 +30,20 @@ YelpCamp
 
 ### RESTFUL ROUTES
 
-| Name  |     url         |   verb  |         description                |
---------|:----------------|:-------:|:----------------------------------:|
-INDEX   |   /dogs         |   GET   |   Display a list of all dogs       |
-NEW     |   /dogs/new     |   GET   |   Displays form to make a new dog  |
-CREATE  |   /dogs         |   POST  |   Add new dog to DB                |
-SHOW    |   /dogs/:id     |   GET   |   Show info about one dog          |
+| Name  |     url                            |   verb  |         description                           |
+--------|:-----------------------------------|:-------:|:---------------------------------------------:|
+INDEX   |   /campgrounds                     |   GET   |   Display a list of all campgrounds           |
+NEW     |   /campgrounds/new                 |   GET   |   Displays form to make a new campground      |
+CREATE  |   /campgrounds                     |   POST  |   Add new campground to DB                    |
+SHOW    |   /campgrounds/:id                 |   GET   |   Show info about one campground              |
+NEW     |   campgrounds/:id/comments/new     |   GET   |   Show comments added to campground           |
+CREATE  |   campgrounds/:id/comments         |   GET   |   Add new comment to DB                       |
 
 
-    
+
+
+
+### Comment New/Create
+- Discuss nested routes
+- Add the comment new and create routes
+- Add the new comment form
--- v2/app.js
+++ v2/app.js
@@ -1,38 +1,17 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const bodyParser = require('body-parser');
+const express = require('express'),
+	app = express(),
+	port = 3000,
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	Campground = require('./models/campground'),
+	Comment = require('./models/comment'),
+	seedDB = require('./seeds');
 
-const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/yelp_camp', { useUnifiedTopology: true, useNewUrlParser: true });
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// SCHEMA SETUP
-const campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String,
-	description: String
-});
-
-let Campground = mongoose.model('Campground', campgroundSchema);
-
-// Campground.create(
-// 	{
-// 		name: 'Granite Hill',
-// 		image: 'https://pixabay.com/get/54e5dc474355a914f1dc84609620367d1c3ed9e04e507440772e7add934ac5_340.jpg',
-// 		description: 'This is a huge granite hill, no bathrooms. No water. Beautiful granite!'
-// 	},
-// 	function(err, campground) {
-// 		if (err) {
-// 			console.log(err);
-// 		} else {
-// 			console.log('NEWLY CREATED CAMPGROUND: ');
-// 			console.log(campground);
-// 		}
-// 	}
-// );
+app.use(express.static(__dirname + '/public'));
+seedDB();
 
 app.get('/', (req, res) => {
 	res.render('landing');
@@ -45,7 +24,7 @@ app.get('/campgrounds', (req, res) => {
 		if (err) {
 			console.log(err);
 		} else {
-			res.render('index', { campgrounds: allCampgrounds });
+			res.render('campgrounds/index', { campgrounds: allCampgrounds });
 		}
 	});
 });
@@ -72,18 +51,53 @@ app.post('/campgrounds', (req, res) => {
 // NEW -  show form to create new campground
 app.get('/campgrounds/new', (req, res) => {
 	// render a new form
-	res.render('new.ejs');
+	res.render('campgrounds/new');
 });
 
 // SHOW - shows more info about
 app.get('/campgrounds/:id', function(req, res) {
 	// find the campground with provided ID
-	Campground.findById(req.params.id, function(err, foundCampground) {
+	Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground) {
 		if (err) {
 			console.log(err);
 		} else {
 			// render show template with that campground
-			res.render('show', { campground: foundCampground });
+			res.render('campgrounds/show', { campground: foundCampground });
+		}
+	});
+});
+
+// ==============================
+// COMMENTS ROUTE
+// ==============================
+
+app.get('/campgrounds/:id/comments/new', function(req, res) {
+	// find campground by id
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('comments/new', { campground: campground });
+		}
+	});
+});
+
+app.post('/campgrounds/:id/comments', function(req, res) {
+	//lookup campground using ID
+	Campground.findById(req.params.id, function(err, campground) {
+		if (err) {
+			console.log(err);
+			res.redirect('/campgrounds');
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if (err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect('/campgrounds/' + campground._id);
+				}
+			});
 		}
 	});
 });
